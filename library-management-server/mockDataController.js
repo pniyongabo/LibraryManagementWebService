560ae4517e615b306d36c82cae88e396393272fc
pniyongabo <patrickniyongabo@gmail.com>
2021-02-24 22:49:26 +0200

    configured and added starter files to node backend

---
+++ library-management-server/mockDataController.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const fs = require('fs');
+
+const basePathToData = path.join(__dirname, 'mockdata');
+
+const getJsonData = function (basePathToData, filename) {
+  var filename = path.join(basePathToData, filename);
+  return JSON.parse(fs.readFileSync(filename, 'utf-8'));
+};
+
+exports.getListOfBooks = function (request, response) {
+  var data = getJsonData(basePathToData, 'book_list_example_one.json');
+  return response.send(data);
+};
---
+++ library-management-server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const fetch = require('node-fetch');
+const path = require('path');
+cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+
+var mockDataController = require('./mockDataController');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/api/books/list', mockDataController.getListOfBooks);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
