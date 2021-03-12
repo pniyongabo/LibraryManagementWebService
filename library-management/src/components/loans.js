0faf1cc2aed53649818a7a9f9c193353b4840ade
pniyongabo <patrickniyongabo@gmail.com>
2021-03-12 10:37:47 +0200

    Added a coming-soon notice on the loans page

--- library-management/src/components/loans.js
+++ library-management/src/components/loans.js
@@ -5,11 +5,16 @@ import Sidebar from "./sidebar";
 
 import "./loans.css";
 
+import book_background from "../assets/book_bkg.png";
+
 export default class Loans extends Component {
   render() {
     return (
       <div>
         <Header />    
+        <div class="header-wrapper">
+            <img class="header-img" src={book_background} alt='book background image' />      
+        </div>
         <Sidebar />
       </div>
     );
