6959311cec7879d4ae2cd600be2284e488b7ae39
Xavier Reed <xreed18@gmail.com>
2021-03-12 16:47:30 -0800

    added libary logo in navbar

--- library-management/src/components/header.js
+++ library-management/src/components/header.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import { Nav, Navbar } from 'react-bootstrap';
+import logo from "../assets/Navbar_logo2.svg"
 export default class Sidebar extends Component {
     render() {
         return (
             <Navbar bg="light" expand="lg">
             <Navbar.Brand href="/">
+                <img
+                    alt="Library logo"
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
                 Library Home
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
