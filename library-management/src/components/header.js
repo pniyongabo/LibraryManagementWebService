a0c7e621138a1384ba7bee676fd82b5baab2d76f
Xavier Reed <xreed18@gmail.com>
2021-03-12 17:20:09 -0800

    changed navbar styling

--- library-management/src/components/header.js
+++ library-management/src/components/header.js
@@ -1,30 +1,32 @@
 import React, { Component } from "react";
 import { Nav, Navbar } from 'react-bootstrap';
 import logo from "../assets/Navbar_logo2.svg"
+import "./header.css"
+
 export default class Sidebar extends Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">
-                <img
-                    alt="Library logo"
-                    src={logo}
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                />{' '}
-                Library Home
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="/books">Books</Nav.Link>
-                <Nav.Link href="/authors">Authors</Nav.Link>
-                <Nav.Link href="/categories">Categories</Nav.Link>
-                <Nav.Link href="/loans">Loans</Nav.Link>
-                <Nav.Link href="/users">Users</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+            <Navbar bg="dark" expand="lg">
+                <Navbar.Brand href="/" className="textStyle">
+                    <img
+                        alt="Library logo"
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-middle"
+                    />{' '}
+                    Library Home
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/books" className="textStyle">Books</Nav.Link>
+                        <Nav.Link href="/authors" className="textStyle">Authors</Nav.Link>
+                        <Nav.Link href="/categories" className="textStyle">Categories</Nav.Link>
+                        <Nav.Link href="/loans" className="textStyle">Loans</Nav.Link>
+                        <Nav.Link href="/users" className="textStyle">Users</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         )
     }
